1980f523346a7615428b85cf77ae291735eb7b45
asus <mha672001@yahoo.com>
2023-07-05 08:42:48 +0330

    #4 get all api

---
+++ API/Controllers/EmployeesController.cs
@@ -0,0 +1,25 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/employees")]
+    public class EmployeesController : Controller
+    {
+        private readonly DataContext _context;
+
+        public EmployeesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetALL()
+        {
+            var employees= await _context.Employees.ToListAsync();
+            return Ok(employees);
+        }
+    }
+}
