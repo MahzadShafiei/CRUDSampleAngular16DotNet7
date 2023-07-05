37fbd7b9648a5ad35a227f053418c5aee4430ace
asus <mha672001@yahoo.com>
2023-07-05 08:15:18 +0330

    #3

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,7 @@
 
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace API
 {
     public class Program
@@ -14,6 +17,7 @@ namespace API
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("EmployeeContext")));
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
